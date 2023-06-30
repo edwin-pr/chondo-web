5405095a358c1d8a6c419f5770de444014841a85
“edwin-pr” <edwingitau100@gmail.com>
2023-06-30 14:55:10 +0300

    nfc

---
+++ chondo web/models.py
@@ -0,0 +1,13 @@
+# models.py
+
+from django.db import models
+
+class NFC_Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
